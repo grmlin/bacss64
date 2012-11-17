ca0454b222c09b528de88e689af1929bff8b37b2
andreas <mail@andreas-wehr.de>
2012-11-17 23:29:00 +0100

    log removed

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ window.addEventListener('load', function () {
     var fileSelector = new FileSelector(document.getElementById('source-select'));
     fileSelector.onChange = function (containers) {
         containers.forEach(function (container) {
-            console.dir(container)
             var img = new ExampleImage(container);
             img.appendTo(document.querySelector('#examples ul'));
         }, this);
